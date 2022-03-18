b5642c4f24f1dc8fde4e49c9ba55e0fc26433233
symons1992 <romanticbao@gmail.com>
2022-03-18 18:38:37 +0800

    first init

---
+++ learn_go_note/page8/main.go
@@ -0,0 +1,32 @@
+package main
+
+// section1
+/*
+func main() {
+	var x int32
+	var s = "hello, world!"
+
+	println(x, s)
+}
+*/
+
+// section2
+/*
+func main() {
+	x := 100
+	println(x)
+}
+*/
+
+// section3
+func main() {
+	x := 100
+
+	if x > 0 {
+		println("x")
+	} else if x < 0 {
+		println("-x")
+	} else {
+		println("0")
+	}
+}
